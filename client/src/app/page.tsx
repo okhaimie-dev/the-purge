ff61383dc73dae388665ccda99aef077f668f560
supreme2580 <victoromorogbe69@gmail.com>
2024-08-03 21:45:29 +0100

    wip

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,19 @@
-import GeneratePromptPage from "@/components/generate-prompt";
+// import GeneratePromptPage from "@/components/generate-prompt";
 
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Earth from "@/components/earth";
+
+// import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <ConnectButton />
-        <h1 className="text-4xl font-bold mb-4">RandomMuse</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between bg-black">
+      <div className="flex flex-col items-center justify-center space-y-5">
+        {/* <ConnectButton /> */}
+        {/* <h1 className="text-4xl font-bold mb-4">RandomMuse</h1>
 
-        <GeneratePromptPage />
+        <GeneratePromptPage /> */}
+        <Earth />
+        <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg">Connect wallet to play</button>
       </div>
     </main>
   );
---
+++ client/src/components/earth.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import ReactPlayer from "react-player";
+
+export default function Earth() {
+    const w_70 = window.innerWidth * 0.70;
+    const h_70 = window.innerHeight * 0.70;
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <ReactPlayer
+                width={`${w_70}px`}
+                height={`${h_70}px`}
+                url="/videos/earth.mp4"
+                controls={false}
+                // light is usefull incase of dark mode
+                light={false}
+                // picture in picture
+                pip={true}
+                playing={true}
+                loop={true}
+                muted={true}
+            />
+            <source src="/videos/earth.mp4" type="video/mp4" />
+            <h3 className="text-5xl sm:text-6xl font-bold text-white">War of Worlds</h3>
+        </div>
+    )
+}
