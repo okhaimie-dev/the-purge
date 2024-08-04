ec026a1e278bd8bb0d556e7c4ba0699d53790394
supreme2580 <victoromorogbe69@gmail.com>
2024-08-04 18:14:55 +0100

    responsiveness

--- client/src/app/play/page.tsx
+++ client/src/app/play/page.tsx
@@ -21,13 +21,13 @@ export default function Play() {
                                     Array.from({ length: 10 }).map((_, i) => <div className="bg-black w-fit h-fit bg-gradient-to-br from-white via-transparent to-white rounded-xl p-0.5">
                                         <div className="bg-black w-fit h-full rounded-xl py-3.5 px-7 flex flex-col items-start justify-center">
                                             <div className="space-y-2.5">
-                                                <div className="flex flex-row items-center justify-center space-x-5">
+                                                <div className="flex flex-row items-center justify-center sm:space-x-5">
                                                     <div className="flex flex-col items-center justify-center space-y-0.5">
                                                         <Image src="https://s2.coinmarketcap.com/static/img/coins/128x128/1.png" alt="earth" width={175} height={175} className="rounded-full border-2 border-white" />
                                                         <h3 className="text-3xl font-bold text-white">Bitcoin</h3>
                                                         <div className="flex flex-row items-center space-x-2.5">
-                                                            <p className="text-white text-lg">Wins: 52</p>
-                                                            <p className="text-white text-lg">Losses: 12</p>
+                                                            <p className="text-white text-lg text-center">Wins: 52</p>
+                                                            <p className="text-white text-lg text-center">Losses: 12</p>
                                                         </div>
                                                     </div>
                                                     <h2 className="text-5xl font-bold text-white">VS</h2>
@@ -35,8 +35,8 @@ export default function Play() {
                                                         <Image src="https://s2.coinmarketcap.com/static/img/coins/128x128/1.png" alt="earth" width={175} height={175} className="rounded-full border-2 border-white" />
                                                         <h3 className="text-3xl font-bold text-white">Bitcoin</h3>
                                                         <div className="flex flex-row items-center space-x-2.5">
-                                                            <p className="text-white text-lg">Wins: 52</p>
-                                                            <p className="text-white text-lg">Losses: 12</p>
+                                                            <p className="text-white text-lg text-center">Wins: 52</p>
+                                                            <p className="text-white text-lg text-center">Losses: 12</p>
                                                         </div>
                                                     </div>
                                                 </div>
