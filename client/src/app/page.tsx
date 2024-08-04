23606c42775374145b41d344d2ea0ea485e053fa
supreme2580 <victoromorogbe69@gmail.com>
2024-08-04 03:06:50 +0100

    wip

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,12 @@
-"use client"
-
 import Earth from "@/components/earth";
-import { useModal } from "connectkit";
-
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
+import HomeConnect from "@/components/home-connect";
 
 export default function Home() {
-  const { setOpen } = useModal();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-black">
       <div className="flex flex-col items-center justify-center space-y-5">
         <Earth />
-        <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg" onClick={() => setOpen(true)}>Connect wallet to play</button>
+        <HomeConnect />
       </div>
     </main>
   );
---
+++ client/src/components/home-connect.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useModal } from "connectkit";
+import { useState } from "react";
+import { useAccount } from "wagmi";
+import { useRouter } from "next/navigation"
+
+export default function HomeConnect() {
+    const { setOpen } = useModal();
+    const account = useAccount();
+    const [profile, setProfile] = useState<{} | undefined>({});
+    const router = useRouter();
+    if (account) {
+        setTimeout(() => {
+            //mock account profile
+            setProfile(undefined);
+            setTimeout(() => {
+                router.push("/world");
+            }, 2000);
+        }, 3000);
+    }
+    return (
+        <>
+            {
+                account ? <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg">{profile === undefined ? "Account not found, redirecting..." : "Checking for account..."}</button>
+                    : <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg" onClick={() => setOpen(true)}>Connect wallet to play</button>
+            }
+        </>
+    )
+}
