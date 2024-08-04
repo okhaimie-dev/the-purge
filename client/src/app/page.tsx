3750cd75e0a0612b210c32617c4e105793c78578
supreme2580 <victoromorogbe69@gmail.com>
2024-08-04 02:46:11 +0100

    wip

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,21 +1,17 @@
 "use client"
 
 import Earth from "@/components/earth";
-import { ConnectKitButton } from "connectkit";
+import { useModal } from "connectkit";
 
 // import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
+  const { setOpen } = useModal();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-black">
       <div className="flex flex-col items-center justify-center space-y-5">
-        {/* <ConnectButton /> */}
-        {/* <h1 className="text-4xl font-bold mb-4">RandomMuse</h1>
-
-        <GeneratePromptPage /> */}
         <Earth />
-        <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg">Connect wallet to play</button>
-        <ConnectKitButton />
+        <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg" onClick={() => setOpen(true)}>Connect wallet to play</button>
       </div>
     </main>
   );
