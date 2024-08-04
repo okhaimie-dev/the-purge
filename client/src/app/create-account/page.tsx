60eb3a0db7505ae16be32ccca608d3ba19a8484d
supreme2580 <victoromorogbe69@gmail.com>
2024-08-04 03:29:51 +0100

    create account

--- client/src/app/create-account/page.tsx
+++ client/src/app/create-account/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
+import { truncateString } from "@/lib/utils";
 import { LegacyRef, useRef } from "react";
+import { useAccount } from "wagmi";
 
 export default function CreateAccount() {
     const ref: LegacyRef<HTMLInputElement> = useRef(null);
+    const { address } = useAccount();
     return (
         <div className="w-full min-h-screen bg-black flex flex-col items-center justify-center">
             <div className="flex flex-col items-center justify-center space-y-5">
                 <h1 className="text-5xl sm:text-6xl font-bold text-white">Create account</h1>
-                <div className="w-[500px] h-[600px] border-2 border-white/50 rounded-lg flex flex-col items-center p-5">
+                <div className="w-[500px] h-fit border-2 border-white/50 rounded-lg flex flex-col items-center p-5 space-y-3.5">
                     <div className="hover:cursor-pointer" onClick={() => ref.current?.click()}>
                         {
                             ref.current?.files ? <img src={URL.createObjectURL(ref.current?.files[0])} alt="profile" className="w-40 h-40 border-2 border-white/50 rounded-full" />
@@ -18,6 +21,12 @@ export default function CreateAccount() {
                         }
                         <input type="file" ref={ref} className="hidden" />
                     </div>
+                    <div className="text-white border-2 border-white/50 rounded-3xl py-1.5 px-3.5">{truncateString(address, 6)}</div>
+                    <input type="text" placeholder="Enter name" className="w-full border-2 border-white/50 rounded-lg p-3.5 bg-transparent outline-0" />
+                    <div className="w-full">
+                        <p className="text-white">Your power: ******</p>
+                    </div>
+                    <button className="border-2 border-white bg-transparent text-white p-2.5 rounded-lg">Create account</button>
                 </div>
             </div>
         </div>
